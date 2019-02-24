306da6fa83eecd0cdded3bd1f85f9df406821c49
dunlop1 <46844352+dunlop1@users.noreply.github.com>
2019-02-24 01:52:20 -0800

    Here is Assignment 1
    Sorry for late

---
+++ AlexeevE/Assignment1-Java/src/main/java/ca/ciccc/Assignment1.java
@@ -0,0 +1,177 @@
+package ca.ciccc;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+public class Assignment1 {
+
+    /**
+     * Write a function to convert temperature from Fahrenheit to Celsius degree
+     * Sample Input : 212
+     *
+     * @param degree
+     * @return "212.0 degree Fahrenheit is equal to 100.0 in Celsius"
+     */
+    public static String fahrenheitToCelsius(double degree) {
+
+        double celsius = (degree - 32) * ((double) 5 / 9);
+        String result = String.format("%.1f degree Fahrenheit is equal to %.1f in Celsius", degree, celsius);
+
+        return result;
+    }
+
+    /**
+     * Write a function program that takes an integer in inches, converts in to meters.
+     * Sample Input : 1000
+     *
+     * @param inches
+     * @return "1000.0 inches is 25.4 meters"
+     */
+    public static String inchesToMeters(int inches) {
+        double meters = inches * 0.0254;
+        String result = String.format("%3.1f inches is %2.1f meters",(float)inches,meters);
+
+        return result;
+    }
+
+    /**
+     * Write a function that takes an integer between 0 and 1000 and adds all the digits.
+     * Sample Input : 565
+     *
+     * @param number
+     * @return "The sum of all digits in 565 is 16"
+     */
+    public static String addDigits(int number) {
+        int num = number;
+        int sum = 0;
+        while(number != 0){
+            sum += number % 10;
+            number /= 10;
+        }
+        String result=String.format("The sum of all digits in %d is %d",num,sum );
+
+        return result;
+    }
+
+    /**
+     * Write a function that takes an integer as minutes and convert it to years and days.
+     * Sample Input : 3456789
+     *
+     * @param mins
+     * @return "3456789 minutes is approximately 6 years and 210 days"
+     */
+    public static String minsToYearsDays(int mins) {
+        int years = mins / 525600;
+        int  daysLeft = mins % 525600;
+        int day = daysLeft / 1440;
+        String result = String.format(mins + " minutes is approximately " + years + " years and " + day + " days");
+
+
+        return result;
+    }
+
+    /**
+     * Write a function that takes weight as Kgs(Kilograms) and height as meters and compute
+     * body mass index (BMI)
+     * Sample Input: kgs = 70, meters = 1.75
+     *
+     * @param kgs
+     * @param meters
+     * @return "Body Mass Index is 22.857"
+     */
+    public static String bmi(int kgs, double meters) {
+        double bmi = kgs/(Math.pow(meters,2));
+        String result = String.format("Body Mass Index is %.3f",bmi);
+
+
+        return result;
+    }
+
+    /**
+     * Write a function that takes a distance (in meters) and the time was taken
+     * (as three numbers: hours, minutes, seconds), and display the speed, in meters
+     * per second, kilometers per hour and miles per hour
+     * (hint: 1 mile = 1609 meters).
+     * Sample Input :
+     * meters = 2500
+     * hours = 5
+     * minutes = 56
+     * seconds = 23
+     *
+     * @param meters
+     * @param hours
+     * @param minutes
+     * @param seconds
+     * @return
+     *      "Your speed in meters/second is 0.1169
+     *       Your speed in km/h is 0.4208
+     *       Your speed in miles/h is 0.2615"
+     */
+    public static String speed(int meters, int hours, int minutes, int seconds) {
+        int totalSeconds=(hours*3600)+(minutes*60)+seconds;
+        double meterPerSecond=(double)meters/(double)totalSeconds;
+        double KilometerPerHour=meterPerSecond*3.6;
+        DecimalFormat df = new DecimalFormat();
+        df.setMinimumFractionDigits(4);
+        df.setRoundingMode(RoundingMode.DOWN);
+
+        String kmPh = df.format(KilometerPerHour);
+        double milesPerHour=meterPerSecond*2.237;
+        // String result=String.format("Your speed in meters/second is ",meterPerSecond,'\n',"Your speed in km/h is ",KilometerPerHour+'\n',"Your speed in miles/h is ",milesPerHour);
+        String result=String.format("Your speed in meters/second is %1.4f\nYour speed in km/h is %s\nYour speed in miles/h is %1.4f",meterPerSecond,kmPh,milesPerHour);
+
+        return result;
+    }
+
+    /**
+     * Write a function that reads a number and display the square,
+     * cube, and the fourth power.
+     * Sample Input :
+     * number = 5
+     *
+     * @param number
+     * @return "Square: 25
+     *          Cube: 125
+     *          Fourth power: 625"
+     */
+    public static String powers(int number) {
+        long square=Math.round(Math.pow(number,2));
+        long cube=Math.round(Math.pow(number,3));
+        long fourth=Math.round(Math.pow(number,4));
+        String result=String.format("Square: %d\nCube: %d\nFourth power: %d",square,cube,fourth);
+
+        return result;
+    }
+
+    /**
+     * Write a function that takes two integers a and b, then prints the sum,
+     * the difference, the product, the average, the distance (absolute value of
+     * the difference), the maximum, and the minimum between the two.
+     * Sample Input :
+     * a = 25
+     * b = 5
+     *
+     * @param a
+     * @param b
+     * @return "Sum of two integers: 30
+     *          Difference of two integers: 20
+     *          Product of two integers: 125
+     *          Average of two integers: 15.00
+     *          Distance of two integers: 20
+     *          Max integer: 25
+     *          Min integer: 5"
+     */
+    public static String arithmetic(int a, int b) {
+        int sum=a+b;
+        int diff=a-b;
+        int product=a*b;
+        double average=(a+b)/2;
+        int dis=Math.abs(diff);
+        int max=0;
+        int min=0;
+        if(a>b){max=a;min=b;}
+        if (a<b){min=a;max=b;}
+        String result=String.format("Sum of two integers: %d\nDifference of two integers: %d\nProduct of two integers: %d\nAverage of two integers: %2.2f\nDistance of two integers: %d\nMax integer: %d\nMin integer: %d",sum,diff,product,average,dis,max,min);
+
+        return result;
+    }
+}
